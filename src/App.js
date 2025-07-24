ac101ed7819336fddf1b70a42ac6c0daffc666f8
gagankumar8294 <gagankumar8294@gmail.com>
2025-07-24 09:13:45 +0530

    customItemContext acts like a provider

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import CustomItemContext, { itemContext } from './itemContext';
 
 function App() {
 
-  const [total, setTotal] = useState(0);
-  const [item, setItem] = useState(0);
   return (
         <div className='App'>
   
