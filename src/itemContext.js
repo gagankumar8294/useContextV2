275acd249a9c20b8d5ddccfcb5a3434436f0a77d
gagankumar8294 <gagankumar8294@gmail.com>
2025-07-24 09:19:02 +0530

    typo error fixed and imported useSTate in itemcontext & used it inside the function rather than using it on the Top level

--- src/itemContext.js
+++ src/itemContext.js
@@ -1,12 +1,13 @@
-import { react , createContext } from "react";
+import { react , createContext, useState } from "react";
 
 const itemContext = createContext();
 
-  const [total, setTotal] = useState(0);
-  const [item, setItem] = useState(0);
-
 function CustomItemContext( {children} ) {
-    reuturn (
+    
+    const [total, setTotal] = useState(0);
+    const [item, setItem] = useState(0);
+    
+    return (
         <itemContext.Provider value={{total, setTotal, item, setItem}}>
             {children}
         </itemContext.Provider>
